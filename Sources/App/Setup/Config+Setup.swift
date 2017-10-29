d872c7311da3366d866688319ed8d9d5457a7a76
Ankur Patel <ankur.patel@ymail.com>
2017-10-29 15:21:51 -0400

    Init

--- Sources/App/Setup/Config+Setup.swift
+++ Sources/App/Setup/Config+Setup.swift
@@ -18,6 +18,7 @@ extension Config {
     /// Add all models that should have their
     /// schemas prepared before the app boots
     private func setupPreparations() throws {
-        preparations.append(Post.self)
+        preparations.append(ShoppingList.self)
+        preparations.append(Item.self)
     }
 }
